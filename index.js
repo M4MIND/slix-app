8bbe3f24cd0bd6a106c8176fc572454476605257
Egor Sysoev <e.sysoev@realtimeboard.com>
2019-04-05 15:36:19 +0500

    Init Project

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let _Slix = require('./dist/Slix')
 let _Container = require('./dist/container/Container')
 
-exports.Slix = _Slix;
-exports.Container = _Container
+exports.Slix = _Slix.default;
+exports.Container = _Container.default;
