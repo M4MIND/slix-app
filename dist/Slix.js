6a37957ae1ef8483deaaec1ede721ddc7605eefa
Egor Sysoev <e.sysoev@realtimeboard.com>
2019-04-05 11:09:03 +0500

    Init Project

---
+++ dist/Slix.js
@@ -0,0 +1,52 @@
+"use strict";
+
+exports.__esModule = true;
+exports["default"] = void 0;
+
+var _Container2 = _interopRequireDefault(require("./container/Container"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+/** @class */
+var Slix =
+/*#__PURE__*/
+function (_Container) {
+  _inherits(Slix, _Container);
+
+  /** @constructor */
+  function Slix() {
+    _classCallCheck(this, Slix);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(Slix).call(this));
+  }
+
+  _createClass(Slix, [{
+    key: "boot",
+    value: function boot() {}
+  }, {
+    key: "run",
+    value: function run() {}
+  }]);
+
+  return Slix;
+}(_Container2["default"]);
+
+exports["default"] = Slix;
---
+++ dist/container/Container.js
@@ -0,0 +1,12 @@
+"use strict";
+
+exports.__esModule = true;
+exports["default"] = void 0;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Container = function Container() {
+  _classCallCheck(this, Container);
+};
+
+exports["default"] = Container;
